986f13aaa8aaff15f1250d4a95650f0360ab5af0
Shouganaii <frias.caio@yahoo.com.br>
2020-01-09 11:34:33 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
+import Bliss from './bliss.jpg'
+import xplogo from './xp-logo.png';
+import { Container, Linha, Coluna } from './componentes/grid/gridArea';
 function App() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    function clock() {
+      setData(new Date().toLocaleTimeString('pt-BR', { hour12: true }))
+      setTimeout(clock, 1000);
+    }
+    clock()
+  }, [data]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Linha width={100} height={95}>
+        <img src={Bliss} style={{ width: '100%', height: '100%', zIndex: '-1' }} alt="bliss" />
+      </Linha>
+      <Linha width={100} height={5} flex="flex-start">
+        <Coluna width={10} height={100} style={{ zIndex: '1', background: '#428ade', minWidth: '140px' }}>
+          <div className="iniciar" >
+            <img src={xplogo} alt="xplogo" />
+            <div>Iniciar</div>
+          </div>
+        </Coluna>
+        <Coluna width={80} height={100} >
+          <div className="barra">
+          </div>
+        </Coluna>
+        <Coluna width={10} height={100} style={{ minWidth: '140px' }}  >
+          <div className="relogio">
+            {data}
+          </div>
+        </Coluna>
+      </Linha>
+    </Container >
   );
 }
 
---
+++ src/componentes/grid/gridArea.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+margin:0;
+width:100vw;
+height:100vh;
+`;
+export const Coluna = styled.div`
+${(props) => props.width ? `width:${props.width}%` : ''}
+${(props) => props.height ? `height:${props.height}%` : ''}
+${(props) => props.flex ? `display:flex;
+ justify-content: ${props.flex};
+ flex-direction:column` : ''}
+`;
+export const Linha = styled.div`
+${(props) => props.width ? `width:${props.width}%` : ''}
+${(props) => props.height ? `height:${props.height}%` : ''}
+${(props) => props.flex ? `display:flex;
+justify-content: ${props.flex};
+flex-direction:row` : ''}
+`;
